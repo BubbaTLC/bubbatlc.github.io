33b6432bf841d1bc7aeabc6e58c88308628af276
Bubba <chabot2464@saskpolytech.ca>
2020-03-27 17:18:21 -0600

    Created secondary branch and added contact page

---
+++ contact/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"></script>
+    <!-- https://bootswatch.com themes: cerulean cosmo cyborg darkly flatly journal litera lumen lux
+	materia	minty pulse sandstone simplex sketchy slate solar spacelab superhero united yeti -->
+    <link rel="stylesheet" href="../bootstrap.min.lux.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <link rel="icon" type="image/x-icon" href="../images/favicons/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" href="../images/favicons/source-code-icon-152-213242.png">
+    <title>Tristan Chabot Projects</title>
+</head>
+<body>
+<div id="navigation">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+        <a class="navbar-brand">Tristan Chabot</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
+                aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../resume">Resume</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../projects">Projects</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="../contact">Contact <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+            <div class="navbar-nav my-2 my-lg-0">
+                <a href="https://github.com/BubbaTLC" class="nav-link mr-sm-2">GitHub <i
+                            class="fab fa-github fa-lg"></i></a>
+                <a href="https://www.linkedin.com/in/tristan-chabot-2b9319181" class="nav-link mr-sm-2">LinkedIn <i
+                            class="fab fa-linkedin fa-lg"></i></a>
+            </div>
+        </div>
+    </nav>
+</div>
+
+<div class="container-sm" id="contact">
+    <form>
+        <div class="form-group">
+            <label for="nameInput">Name</label>
+            <input type="text" class="form-control" id="nameInput" maxlength="25" minlength="1" placeholder="Bubba Chabot">
+        </div>
+    </form>
+</div>
+
+</body>
+</html>
