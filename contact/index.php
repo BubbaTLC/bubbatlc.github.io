ac029ab3d7624bad7d81441f848bd807f614d0c8
Bubba <chabot2464@saskpolytech.ca>
2020-03-28 00:56:00 -0600

    Added PHPMailer. Was able to send myself an email! - Bubba Chabot

--- contact/index.php
+++ contact/index.php
@@ -1,3 +1,47 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+require '../vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    // Declare variables
+    $name = "";
+    $company = "";
+    $message = "";
+
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'chabot.tristan@gmail.com';                     // SMTP username
+    $mail->Password   = 'Oilers12101119';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('chabot.tristan@gmail.com', $name);
+    $mail->addAddress('chabot.tristan@gmail.com', 'Bubba');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Portfolio Contact' . $company;
+    $mail->Body    = $message;
+
+    //$mail->send();
+    $mail->smtpClose();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -58,10 +102,10 @@
                    placeholder="Bubba Chabot">
         </div>
 
-        <div class="form-group">
-            <label for="emailInput">Email</label>
-            <input type="email" class="form-control" id="emailInput" required placeholder="example@mail.com">
-        </div>
+<!--        <div class="form-group">-->
+<!--            <label for="emailInput">Email</label>-->
+<!--            <input type="email" class="form-control" id="emailInput" required placeholder="example@mail.com">-->
+<!--        </div>-->
 
         <div class="form-group">
             <label for="orgInput">Organization</label>
@@ -73,11 +117,8 @@
             <label for="msgInput">Message</label>
             <textarea class="form-control" id="msgInput" maxlength="1000" placeholder="Got some questions?"></textarea>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary" onclick="">Submit</button>
     </form>
-
-    <!--    Comment/Message-->
 </div>
-
 </body>
 </html>
